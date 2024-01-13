2f5d065d21b6639072376c4f45b76fbf065ee7e4
naowmi <vadimpelyushenko@gmail.com>
2024-01-13 18:18:08 +0200

    com

---
+++ src/js/1-timer.js
@@ -0,0 +1,82 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+let userSelectedDate
+const daysDisplay = document.querySelector("[data-days]")
+const hoursDisplay = document.querySelector("[data-hours]")
+const minutesDisplay = document.querySelector("[data-minutes]")
+const secondsDisplay = document.querySelector("[data-seconds]")
+const startButton = document.querySelector('.start-btn');
+startButton.disabled = true
+const options = {
+  enableTime: true,
+    time_24hr: true,
+  defaultDate: new Date(),
+    minuteIncrement: 1,
+    
+    onClose(selectedDates) {
+      const selectedDate = selectedDates[0]
+        if (selectedDate < new Date()) {
+            butterup.toast({
+            title:'ERROR',
+            message:'Please choose a date in the future',
+            type:'error',
+              icon: true, 
+            })
+            startButton.disabled = true
+        } else {
+            startButton.disabled = false
+            userSelectedDate = selectedDate
+            console.log(userSelectedDate);
+        }
+  },
+};
+flatpickr("#datetime-picker", options)
+
+let intervalId
+startButton.addEventListener("click", () => {
+    startButton.disabled = true
+    intervalId = setInterval(updateTimer, 1000);
+
+})
+function updateTimer() {
+    const timer = userSelectedDate - new Date();
+    if (timer <= 0) {
+        clearInterval(intervalId);
+        daysDisplay.textContent = "00";
+        hoursDisplay.textContent = "00";
+        minutesDisplay.textContent = "00";
+        secondsDisplay.textContent = "00";
+        return;
+    }
+    const { days, hours, minutes, seconds } = convertMs(timer);
+daysDisplay.textContent = addLeadingZero(days),
+  hoursDisplay.textContent = addLeadingZero(hours),
+  minutesDisplay.textContent = addLeadingZero(minutes),
+secondsDisplay.textContent = addLeadingZero(seconds)
+
+}
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+  
+function addLeadingZero(value) {
+    return value.toString().padStart(2, "0");
+}
+
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,36 @@
+import iziToast from "izitoast";
+const form = document.querySelector(".form")
+const delayInput = document.querySelector("input[name=delay]")
+const fulfilledRadio = document.querySelector("input[value=fulfilled]")
+const rejectedRadio = document.querySelector("input[value=rejected]")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const delay = parseInt(delayInput.value);
+    delayInput.value = ""
+    const getPromise = new Promise((res, rej) => {
+        setTimeout(() => {
+            if (fulfilledRadio.checked) {
+                res(delay);
+            } else if (rejectedRadio.checked) {
+                rej(delay);
+            }
+        }, delay)
+    });
+
+    getPromise.then((delay) => {
+        butterup.toast({
+            title: "Success",
+            type: "success",
+            message: `✅ Fulfilled promise in ${delay}ms`
+        });
+            
+    })
+        .catch((delay) => {
+            butterup.toast({
+                title: "Error",
+                type: "error",
+                message: `❌ Rejected promise in ${delay}ms`
+           })
+        });
+});
